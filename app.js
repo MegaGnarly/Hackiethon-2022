f6157ac1f7b11e5f1c0f532d02c9d851d88df73e
MegaGnarly <davidliu0115@gmail.com>
2022-09-26 17:32:38 +1000

    added template with working css

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const path = require('path');
 
 const app = express();
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'))
 })
+
+app.use(express.static('public'));
+
 
 app.listen(process.env.PORT || PORT, () => {
     console.log('\n\nWebpage is running!')
