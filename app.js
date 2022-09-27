52d91572a67239ce8c82eb5f87f99472061057e2
MegaGnarly <davidliu0115@gmail.com>
2022-09-27 13:20:28 +1000

    added more pages

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/homepage', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'))
 })
 
-app.get('/homepage', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/homepage1.html'))
+})
+
+app.get('/profile', (req, res) => {
+    res.sendFile(path.join(__dirname+'/profile.html'))
 })
 
 app.use(express.static('public'));
